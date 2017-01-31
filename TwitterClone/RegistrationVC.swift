911f933fc1f71422e67b680dbe43a4833f9b9ef3
Christine Cunniff <christinecunniff@gmail.com>
2017-01-31 12:42:27 -0500

    Added email field and connected the outlets and actions.

---
+++ TwitterClone/RegistrationVC.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class RegistrationVC: UIViewController {
+
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var lastNameField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func registerTapped(_ sender: Any) {
+    }
+   
+    @IBOutlet weak var signInTapped: UIButton!
+
+}
+
