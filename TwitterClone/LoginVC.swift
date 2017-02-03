95f9432a661a65f3f90234bcbec2eca1b4263d6a
Christine Cunniff <christinecunniff@gmail.com>
2017-02-03 16:27:40 -0500

    Accessed loging PHP information via json to allow registered users to login.

--- TwitterClone/LoginVC.swift
+++ TwitterClone/LoginVC.swift
@@ -16,7 +16,54 @@ class LoginVC: UIViewController {
             usernameField.attributedPlaceholder = NSAttributedString(string: "Enter username", attributes: [NSForegroundColorAttributeName: UIColor.red])
             passwordField.attributedPlaceholder = NSAttributedString(string: "Enter password", attributes: [NSForegroundColorAttributeName: UIColor.red])
         } else {
+            
+            // shortcuts
+            let username = usernameField.text!.lowercased()
+            let password = passwordField.text!
+            
             // send request to mysql db
+            // url to access our php file
+            let url = NSURL(string: "http://localhost/TwitterClone/login.php")!
+            
+            // request url
+            let request = NSMutableURLRequest(url: url as URL)
+            
+            // method to pass data POST- it is secured
+            request.httpMethod = "POST"
+            
+            // body appended to url (won't be seen because we are using post)
+            let body = "username=\(username)&password=\(password)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            // launch session
+            URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                
+                if error == nil {
+                    
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                        
+                        guard let parseJSON = json else {
+                            print("Error while parsing")
+                            return
+                        }
+                        
+                        print(parseJSON)
+                        
+                        let id = parseJSON["id"] as? String
+                        
+                        if id != nil {
+                            // successfully logged in
+                        }
+                    } catch {
+                        print("Caught an error: \(error)")
+                    }
+                    
+                } else {
+                    print("Error: \(error)")
+                }
+                
+            }).resume()
         }
         
     }
