74dac771a560dd2b7bc773a78ade3c1fe7e80988
Christine Cunniff <christinecunniff@gmail.com>
2017-02-09 16:09:39 -0500

    Customized tab bar icons.

--- TwitterClone/AppDelegate.swift
+++ TwitterClone/AppDelegate.swift
@@ -5,6 +5,7 @@ let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
 
 let redSmoothColor = UIColor(red: 255/255, green: 50/255, blue: 75/255, alpha: 1)
 let lightGreenSmoothColor = UIColor(red: 230/255, green: 244/255, blue: 125/255, alpha: 1)
+let graySmoothColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
 let brandBlueColor = UIColor(red: 45 / 255, green: 213 / 255, blue: 255 / 255, alpha: 1)
 
 let fontSize12 = UIScreen.main.bounds.width / 31
--- TwitterClone/TabBarVC.swift
+++ TwitterClone/TabBarVC.swift
@@ -12,6 +12,13 @@ class TabBarVC: UITabBarController {
         UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for: .normal)
         UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.white], for: .selected)
         
+        // sets colors for each icon in the tab bar
+        for item in self.tabBar.items! as [UITabBarItem] {
+            if let image = item.image {
+                item.image = image.imageColor(color: graySmoothColor).withRenderingMode(.alwaysOriginal)
+            }
+        }
+        
     }
 
 }
@@ -23,11 +30,13 @@ extension UIImage {
         UIGraphicsBeginImageContextWithOptions(self.size, false, scale)
         let context = UIGraphicsGetCurrentContext()! as CGContext
         context.translateBy(x: 0, y: self.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
         
         let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
         context.clip(to: rect, mask: self.cgImage!)
         
         color.setFill()
+        context.fill(rect)
         
         let newImage = UIGraphicsGetImageFromCurrentImageContext()! as UIImage
         UIGraphicsEndImageContext()
