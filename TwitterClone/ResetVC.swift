7bb75bc059fb009804f41879d30df02ac2d2a454
Christine Cunniff <christinecunniff@gmail.com>
2017-02-09 10:03:21 -0500

    Corrected IBAction connection. Parsed json from php to reset the password via the app.

--- TwitterClone/ResetVC.swift
+++ TwitterClone/ResetVC.swift
@@ -4,18 +4,57 @@ class ResetVC: UIViewController {
 
     @IBOutlet weak var emailField: UITextField!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
     }
 
     @IBAction func resetTapped(_ sender: Any) {
         if emailField.text!.isEmpty {
             emailField.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSForegroundColorAttributeName: UIColor.red])
         } else {
+            
+            let email = emailField.text!.lowercased()
+            
             // if text is entered, send mysql/php/hosting request
+            
+            // url path to php file
+            let url = NSURL(string: "http://localhost/TwitterClone/resetPassword.php")!
+            
+            // request to send this file
+            let request = NSMutableURLRequest(url: url as URL)
+            
+            // method of passing info to this file
+            request.httpMethod = "POST"
+            
+            // body to be appended to URL - it passes info to our php file
+            let body = "email=\(email)"
+            request.httpBody = body.data(using: String.Encoding.utf8)
+            
+            URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                
+                if error == nil {
+                
+                    // give main queue to UI to communicate back
+                    DispatchQueue.main.async(execute: {
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as? NSDictionary
+                            
+                            guard let parseJSON = json else {
+                                print("Error while parsing")
+                                return
+                            }
+                            print(parseJSON)
+                            
+                        } catch {
+                            print("Caught an error: \(error)")
+                        }
+                    })
+                    
+                } else {
+                    print("Error: \(error)")
+                }
+            }).resume()
+            
         }
     }
 }
