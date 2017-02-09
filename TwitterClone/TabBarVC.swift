e619bed52e94c97f6afc0db27d00c244363cb80a
Christine Cunniff <christinecunniff@gmail.com>
2017-02-09 15:55:38 -0500

    Customized icons in the tab bar

--- TwitterClone/TabBarVC.swift
+++ TwitterClone/TabBarVC.swift
@@ -5,9 +5,34 @@ class TabBarVC: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.tabBar.tintColor = UIColor.white
+        self.tabBar.barTintColor = brandBlueColor
+        self.tabBar.isTranslucent = false
+        
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.black], for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.white], for: .selected)
+        
     }
 
+}
 
+extension UIImage {
 
+    func imageColor(color: UIColor) {
+        
+        UIGraphicsBeginImageContextWithOptions(self.size, false, scale)
+        let context = UIGraphicsGetCurrentContext()! as CGContext
+        context.translateBy(x: 0, y: self.size.height)
+        
+        let rect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        context.clip(to: rect, mask: self.cgImage!)
+        
+        color.setFill()
+        
+        let newImage = UIGraphicsGetImageFromCurrentImageContext() as UIImage
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+    
 }
